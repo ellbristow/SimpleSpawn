f3f8f107c05c5e191ea13808c23091012967d17e
Elliott Bristow <admin@jigsawspain.com>
2012-02-08 00:50:57 +0100

    == 1.1.0 == Added: Configurable teleport effects Fixed (hopefully): Bug when setting home/spawn on a slab Changed: Renamed users.yml to locations.yml Changed: Moved event listeners to main class Changed: Simplified event listeners Changed: Cleaned up some areas of the code

--- src/me/ellbristow/SimpleSpawn/SimpleSpawn.java
+++ src/me/ellbristow/SimpleSpawn/SimpleSpawn.java
@@ -2,10 +2,9 @@ package me.ellbristow.SimpleSpawn;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Effect;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.command.Command;
@@ -13,31 +12,42 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerBedEnterEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class SimpleSpawn extends JavaPlugin {
+public class SimpleSpawn extends JavaPlugin implements Listener {
 	
 	public static SimpleSpawn plugin;
-	public final Logger logger = Logger.getLogger("minecraft");
-	public final homeListener homeListener = new homeListener(this);
+	protected FileConfiguration config;
 	public FileConfiguration usersConfig = null;
 	public File usersFile = null;
+	public int tpEffect = 3; // 3 = off
 
 	@Override
 	public void onDisable() {
-		PluginDescriptionFile pdfFile = this.getDescription();
-		logger.info("[" + pdfFile.getName() + "] is disabled.");		
+		
 	}
 
 	@Override
 	public void onEnable() {
-		PluginDescriptionFile pdfFile = this.getDescription();
-		logger.info("[" + pdfFile.getName() + "] version " + pdfFile.getVersion() + " is now enabled.");
+		config = getConfig();
+		boolean useTpEffect = config.getBoolean("use_teleport_effect", true);
+		config.set("use_teleport_effect", useTpEffect);
+		if (useTpEffect) {
+			tpEffect = config.getInt("teleport_effect_type", 1);
+			config.set("teleport_effect_type", tpEffect);
+		} else {
+			config.set("teleport_effect_type", config.getInt("teleport_effect_type", 1));
+		}
+		saveConfig();
 		usersConfig = this.getUsersConfig();
 		PluginManager pm = getServer().getPluginManager();
-		pm.registerEvents(homeListener, this);
+		pm.registerEvents(this, this);
 	}
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
@@ -49,7 +59,9 @@ public class SimpleSpawn extends JavaPlugin {
 			Player player = (Player) sender;
 			if (player.hasPermission("simplespawn.set")) {
 				String world = player.getWorld().getName();
-				getServer().getWorld(world).setSpawnLocation((int)player.getLocation().getX(), (int)player.getLocation().getY(), (int)player.getLocation().getZ());
+				usersConfig.set(world + ".x", (double)player.getLocation().getX());
+				usersConfig.set(world + ".y", (double)player.getLocation().getY());
+				usersConfig.set(world + ".z", (double)player.getLocation().getZ());
 				usersConfig.set(world + ".yaw", (double)player.getLocation().getYaw());
 				usersConfig.set(world + ".pitch", (double)player.getLocation().getPitch());
 				saveUsersConfig();
@@ -65,13 +77,13 @@ public class SimpleSpawn extends JavaPlugin {
 			Player player = (Player) sender;
 			if (player.hasPermission("simplespawn.use")) {
 				String world = player.getWorld().getName();
-				Location spawnLoc = getServer().getWorld(world).getSpawnLocation();
-				float worldYaw = (float)usersConfig.getDouble(world + ".yaw", player.getWorld().getSpawnLocation().getYaw());
-				float worldPitch = (float)usersConfig.getDouble(world + ".pitch", player.getWorld().getSpawnLocation().getPitch());
-				spawnLoc.setYaw(worldYaw);
-				spawnLoc.setPitch(worldPitch);
-				player.teleport(spawnLoc);
-				player.sendMessage(ChatColor.GOLD + "WHOOSH!");
+				double spawnX = usersConfig.getDouble(world + ".x", player.getWorld().getSpawnLocation().getX());
+				double spawnY = usersConfig.getDouble(world + ".y", player.getWorld().getSpawnLocation().getY());
+				double spawnZ = usersConfig.getDouble(world + ".z", player.getWorld().getSpawnLocation().getZ());
+				float spawnYaw = (float)usersConfig.getDouble(world + ".yaw", player.getWorld().getSpawnLocation().getYaw());
+				float spawnPitch = (float)usersConfig.getDouble(world + ".pitch", player.getWorld().getSpawnLocation().getPitch());
+				Location spawnLoc = new Location(player.getWorld(), spawnX, spawnY, spawnZ, spawnYaw, spawnPitch);
+				simpleTeleport(player, spawnLoc);
 				return true;
 			}
 		}
@@ -82,14 +94,7 @@ public class SimpleSpawn extends JavaPlugin {
 			}
 			Player player = (Player) sender;
 			if (player.hasPermission("simplespawn.home.set")) {
-				usersConfig.set(player.getName().toLowerCase() + ".world", player.getWorld().getName());
-				usersConfig.set(player.getName().toLowerCase() + ".x", player.getLocation().getX());
-				usersConfig.set(player.getName().toLowerCase() + ".y", player.getLocation().getY());
-				usersConfig.set(player.getName().toLowerCase() + ".z", player.getLocation().getZ());
-				usersConfig.set(player.getName().toLowerCase() + ".yaw", (double)player.getLocation().getYaw());
-				usersConfig.set(player.getName().toLowerCase() + ".pitch", (double)player.getLocation().getPitch());
-				saveUsersConfig();
-				player.sendMessage(ChatColor.GOLD + "Your home has been set to this location!");
+				setHomeLoc(player);
 				return true;
 			}
 		}
@@ -100,24 +105,74 @@ public class SimpleSpawn extends JavaPlugin {
 			}
 			Player player = (Player) sender;
 			if (player.hasPermission("simplespawn.home.use")) {
-				World homeWorld = getServer().getWorld(usersConfig.getString(player.getName().toLowerCase() + ".world", getServer().getWorlds().get(0).getName()));
-				double homeX = usersConfig.getDouble(player.getName().toLowerCase() + ".x", player.getWorld().getSpawnLocation().getX());
-				double homeY = usersConfig.getDouble(player.getName().toLowerCase() + ".y", player.getWorld().getSpawnLocation().getY());
-				double homeZ = usersConfig.getDouble(player.getName().toLowerCase() + ".z", player.getWorld().getSpawnLocation().getZ());
-				float homeYaw = (float)usersConfig.getDouble(player.getName().toLowerCase() + ".yaw", player.getWorld().getSpawnLocation().getYaw());
-				float homePitch = (float)usersConfig.getDouble(player.getName().toLowerCase() + ".pitch", player.getWorld().getSpawnLocation().getPitch());
-				Location homeLoc = new Location(homeWorld, homeX, homeY, homeZ, homeYaw, homePitch);
-				player.teleport(homeLoc);
-				player.sendMessage(ChatColor.GOLD + "WHOOSH!");
+				Location homeLoc = getHomeLoc(player);
+				simpleTeleport(player, homeLoc);
 				return true;
 			}
 		}
 		return false;
 	}
 	
+	public void simpleTeleport(Player player, Location loc) {
+		Location leftLoc = player.getLocation();
+		switch (tpEffect) {
+		case 0:
+			player.getWorld().strikeLightningEffect(leftLoc);
+			loc.getWorld().strikeLightningEffect(loc);
+		break;
+		default:
+			leftLoc.setY(leftLoc.getY() + 1);
+			Location newLoc = new Location(loc.getWorld(), loc.getX(), loc.getY(), loc.getZ(), loc.getYaw(), loc.getPitch());
+			newLoc.setY(newLoc.getY() + 1);
+			switch (tpEffect) {
+			case 1:
+				leftLoc.getWorld().playEffect(leftLoc, Effect.ENDER_SIGNAL, 0);
+				newLoc.getWorld().playEffect(newLoc, Effect.ENDER_SIGNAL, 0);
+			break;
+			case 2:
+				leftLoc.getWorld().playEffect(leftLoc, Effect.SMOKE, 0);
+				newLoc.getWorld().playEffect(newLoc, Effect.SMOKE, 0);
+			break;
+			case 3:
+				leftLoc.getWorld().playEffect(leftLoc, Effect.MOBSPAWNER_FLAMES, 0);
+				newLoc.getWorld().playEffect(newLoc, Effect.MOBSPAWNER_FLAMES, 0);
+			break;
+			}
+		break;
+		}
+		player.teleport(loc);
+		player.sendMessage(ChatColor.GOLD + "WHOOSH!");
+	}
+	
+	public void setHomeLoc(Player player) {
+		Location homeLoc = player.getLocation();
+		homeLoc.setYaw(player.getLocation().getYaw());
+		homeLoc.setPitch(player.getLocation().getPitch());
+		player.setBedSpawnLocation(homeLoc);
+		usersConfig.set(player.getName().toLowerCase() + ".world", homeLoc.getWorld().getName());
+		usersConfig.set(player.getName().toLowerCase() + ".x", homeLoc.getX());
+		usersConfig.set(player.getName().toLowerCase() + ".y", homeLoc.getY());
+		usersConfig.set(player.getName().toLowerCase() + ".z", homeLoc.getZ());
+		usersConfig.set(player.getName().toLowerCase() + ".yaw", homeLoc.getYaw());
+		usersConfig.set(player.getName().toLowerCase() + ".pitch", homeLoc.getPitch());
+		saveUsersConfig();
+		player.sendMessage(ChatColor.GOLD + "Your home has been set to this location!");
+	}
+	
+	public Location getHomeLoc(Player player) {
+		World homeWorld = getServer().getWorld(usersConfig.getString(player.getName().toLowerCase() + ".world", player.getWorld().getName()));
+		double homeX = usersConfig.getDouble(player.getName().toLowerCase() + ".x", player.getWorld().getSpawnLocation().getX());
+		double homeY = usersConfig.getDouble(player.getName().toLowerCase() + ".y", player.getWorld().getSpawnLocation().getY());
+		double homeZ = usersConfig.getDouble(player.getName().toLowerCase() + ".z", player.getWorld().getSpawnLocation().getZ());
+		float homeYaw = (float)usersConfig.getDouble(player.getName().toLowerCase() + ".yaw", player.getWorld().getSpawnLocation().getYaw());
+		float homePitch = (float)usersConfig.getDouble(player.getName().toLowerCase() + ".pitch", player.getWorld().getSpawnLocation().getPitch());
+		Location homeLoc = new Location(homeWorld, homeX, homeY, homeZ, homeYaw, homePitch);
+		return homeLoc;
+	}
+	
 	public void loadUsersConfig() {
 		if (usersFile == null) {
-			usersFile = new File(getDataFolder(),"users.yml");
+			usersFile = new File(getDataFolder(),"locations.yml");
 		}
 		usersConfig = YamlConfiguration.loadConfiguration(usersFile);
 	}
@@ -136,7 +191,20 @@ public class SimpleSpawn extends JavaPlugin {
 		try {
 			usersConfig.save(usersFile);
 		} catch (IOException ex) {
-			logger.log(Level.SEVERE, "Could not save " + this.usersFile, ex );
+			getLogger().severe("Could not save " + usersFile + "!");
 		}
+	}
+	
+	/* EVENT LISTENERS */
+	
+	@EventHandler (priority = EventPriority.NORMAL)
+	public void onPlayerRespawn (PlayerRespawnEvent event) {
+		Location homeLoc = getHomeLoc(event.getPlayer());
+		event.setRespawnLocation(homeLoc);
+	}
+	
+	@EventHandler (priority = EventPriority.NORMAL)
+	public void onPlayerBedEnter (PlayerBedEnterEvent event) {
+		setHomeLoc(event.getPlayer());
 	}
 }
